090ce5697fb66ede5cffdcaa55e01b3a905eaf11
Mohammad Shafqat Siddiqui <mohdshafqatsiddiqui1@gmail.com>
2026-02-09 13:18:50 +0530

    Feat: Add public routes and register them in main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from app.public.router import router as public_router
 from app.users.router import router as users_router
 
 app = FastAPI()
+app.include_router(public_router)
 app.include_router(users_router)
