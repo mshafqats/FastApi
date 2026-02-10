873bcb5868bc8c61e30e6bb6dbe2ef6d4779b5f3
Mohammad Shafqat Siddiqui <mohdshafqatsiddiqui1@gmail.com>
2026-02-10 16:29:10 +0530

    Feat: Improve user experience with a visually appealing question answering page

--- app/public/router.py
+++ app/public/router.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
-@router.get("/")
-async def root():
-    return { "message": "Hello World" }
+@router.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("ask.html", {"request": request})
